dc83f74cc091ba26bf126a4bfff0cb40a4cce8a3
JulhanAbdulMalik-TI22A <114629579+JulhanAbdulMalik-TI22A@users.noreply.github.com>
2023-10-01 15:43:37 +0700

    Add files via upload

---
+++ Tugas_Sesi ke 2_Julhan Abdul Malik.js
@@ -0,0 +1,13 @@
+var firstName = "Julhan ";
+var lastName = "A Malik";
+var age = 20;
+var isMarried = false;
+
+console.log("Halo, Nama saya " + firstName + lastName);
+console.log("Saya berusia " + age + " tahun");
+
+if (isMarried) {
+    console.log("Saya sudah Menikah");
+} else {
+    console.log("Saya belum Menikah dan masih menikmati hidup sendiri hhe");
+}
